3466b89fbc91120e03a579b4e885c0097f0824c4
krishnapranavv <pranavmanchala1@gmail.com>
2025-06-13 13:59:01 +0400

    pandas added

--- housingprices.py
+++ housingprices.py
@@ -1,8 +1,15 @@
 import mysql.connector as ms
+import pandas as pd
 mydb=ms.connect(host='localhost',user='root',passwd='12345',database='Prices')
 print("Connection has been successfully established !")
-mycursor=mydb.cursor()
-mycursor.execute('select * from housing')
-mydata=mycursor.fetchall()
-print(mydata)
+cursor=mydb.cursor()
+query="SELECT price,year FROM housing WHERE location = %s "
+location_filter='hyderabad'
+cursor.execute(query,(location_filter,))
+data=cursor.fetchall()
+columns=[i[0] for i in cursor.description]
+df= pd.DataFrame(data,columns=columns)
 mydb.close()
+
+
+
